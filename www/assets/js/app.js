3fd762114e01383d77e33994329e53273b7ff85c
Tom Rochette <roctom@gmail.com>
2015-05-17 19:10:33 -0400

    [#1] Setup reminders [#3] Open data form when clicking on a notification
    Replace $index based url with item.id based urls.
    Item ids are uuid v4.

--- www/assets/js/app.js
+++ www/assets/js/app.js
@@ -26,13 +26,14 @@ dtReminderApp.config(function($stateProvider, $urlRouterProvider) {
 		$urlRouterProvider.otherwise('/home');
 });
 
-dtReminderApp.controller('ItemIndexController', function($scope, ItemService) {
+dtReminderApp.controller('ItemIndexController', function($scope, ItemService, ReminderService) {
 	$scope.shouldShowDelete = false;
 	$scope.shouldShowReorder = false;
 	$scope.items = ItemService.all();
 
-	$scope.removeItem = function(index) {
-		ItemService.delete(index);
+	$scope.removeItem = function(item) {
+		ReminderService.cancelReminder(item);
+		ItemService.delete(item);
 		$scope.items = ItemService.all();
 	};
 
@@ -49,7 +50,13 @@ dtReminderApp.controller('ItemIndexController', function($scope, ItemService) {
 
 dtReminderApp.controller('ItemFormController', function($scope) {
 	$scope.item = {
+		id: uuidv4(),
 		name: null,
+		reminder: {
+			id: null,
+			frequency: null,
+			startAt: new Date(),
+		},
 		metrics: [],
 	};
 
@@ -63,7 +70,7 @@ dtReminderApp.controller('ItemFormController', function($scope) {
 	};
 });
 
-dtReminderApp.controller('ItemCreateController', function($controller, $scope, $stateParams, $ionicHistory, ItemService) {
+dtReminderApp.controller('ItemCreateController', function($controller, $scope, $stateParams, $ionicHistory, ItemService, ReminderService) {
 	$controller('ItemFormController', {
 		$scope: $scope,
 	});
@@ -71,12 +78,13 @@ dtReminderApp.controller('ItemCreateController', function($controller, $scope, $
 	$scope.addMetric();
 
 	$scope.save = function() {
+		ReminderService.configureReminder($scope.item);
 		ItemService.push($scope.item);
 		$ionicHistory.goBack();
 	};
 });
 
-dtReminderApp.controller('ItemEditController', function($controller, $scope, $stateParams, $ionicHistory, ItemService) {
+dtReminderApp.controller('ItemEditController', function($controller, $scope, $stateParams, $ionicHistory, ItemService, ReminderService) {
 	$controller('ItemFormController', {
 		$scope: $scope,
 	});
@@ -84,11 +92,15 @@ dtReminderApp.controller('ItemEditController', function($controller, $scope, $st
 	var id = $stateParams.itemId;
 	console.log('Editing ' + id);
 
-	$scope.item = ItemService.get(id);
+	angular.extend($scope.item, ItemService.findById(id) || {});
+
+	// When we deserialize the object for display, the startAt field is stored as a string and must be converted to a Date object for angular to properly manage it.
+	$scope.item.reminder.startAt = $scope.item.reminder.startAt ? new Date($scope.item.reminder.startAt) : null;
 
 	console.log($scope.item);
 
 	$scope.save = function() {
+		ReminderService.configureReminder($scope.item);
 		ItemService.set(id, $scope.item);
 		$ionicHistory.goBack();
 	};
@@ -97,7 +109,7 @@ dtReminderApp.controller('ItemEditController', function($controller, $scope, $st
 dtReminderApp.controller('ItemDataCreateController', function($controller, $scope, $stateParams, $ionicHistory, ItemService, DataTrackerService) {
 	var id = $stateParams.itemId;
 
-	$scope.item = ItemService.get(id);
+	$scope.item = ItemService.findById(id);
 	$scope.data = [];
 
 	angular.forEach($scope.item.metrics, function(metric) {
@@ -140,6 +152,12 @@ dtReminderApp.service('ItemService', function($rootScope) {
 		return items;
 	};
 
+	this.findById = function(id) {
+		return _.find(items, function(item) {
+			return item.id === id;
+		});
+	};
+
 	this.get = function(index) {
 		return angular.copy(items[index]);
 	};
@@ -154,7 +172,13 @@ dtReminderApp.service('ItemService', function($rootScope) {
 		persist();
 	}
 
-	this.delete = function(index) {
+	this.delete = function(item) {
+		var index = _.findIndex(items, function(testedItem) {
+			return testedItem.id === item.id;
+		});
+		if (index === -1) {
+			return;
+		}
 		var item = items.splice(index, 1);
 		persist();
 		return item;
@@ -214,6 +238,84 @@ dtReminderApp.service('DataTrackerService', function($q, $http) {
 	};
 });
 
+cordova = typeof cordova !== 'undefined' ? cordova : {};
+cordova.plugins = cordova.plugins || {};
+
+dtReminderApp.service('ReminderService', function($state) {
+	var self = this;
+
+	// TODO: Queue function calls until device is ready?
+	var notification = {
+		local: {
+			schedule: function() { console.log('ReminderService.schedule'); },
+			update: function() { console.log('ReminderService.update'); },
+			cancel: function() { console.log('ReminderService.cancel'); },
+			on: function() { console.log('ReminderService.on'); },
+		},
+	};
+
+	document.addEventListener('deviceready', function() {
+		notification = cordova.plugins.notification;
+
+		notification.local.on('click', function(message) {
+			var data = JSON.parse(message.data);
+			$state.go('item-data-create', { itemId: data.itemId });
+		});
+	});
+
+	this.configureReminder = function(item) {
+		var data = {
+			id: item.reminder.id,
+			text: item.name,
+			every: item.reminder.frequency,
+			at: item.reminder.startAt,
+			data: { itemId: item.id },
+		};
+
+		if (item.reminder.id && ! item.reminder.frequency) {
+			self.cancelReminder(item);
+		} else if (item.reminder.id) {
+			self.updateReminder(item, data);
+		} else if (item.reminder.frequency) {
+			self.scheduleReminder(item, data);
+		}
+	};
+
+	this.getNewReminderId = function() {
+		var redminderId = parseInt(localStorage['reminderId'] || 1, 10);
+		localStorage['reminderId'] = redminderId + 1;
+		return redminderId;
+	};
+
+	this.cancelReminder = function(item) {
+		if ( ! item.reminder.id) {
+			return;
+		}
+
+		console.log('Cancelling redminder id = ' + item.reminder.id);
+		notification.local.cancel(item.reminder.id);
+		item.reminder.id = null;
+	};
+
+	this.updateReminder = function(item, data) {
+		if ( ! item.reminder.id) {
+			return;
+		}
+
+		// TODO: Verify if notification still exists... If not, call scheduleReminder
+
+		console.log('Updating redminder id = ' + item.reminder.id);
+		notification.local.update(data);
+	};
+
+	this.scheduleReminder = function(item, data) {
+		item.reminder.id = self.getNewReminderId();
+		data.id = item.reminder.id;
+		console.log('Scheduling redminder id = ' + item.reminder.id);
+		notification.local.schedule(data);
+	};
+});
+
 window.plugins = window.plugins || {};
 
 var showToast = function(message, duration, position) {
@@ -223,3 +325,26 @@ var showToast = function(message, duration, position) {
 	var toast = window.plugins.toast || {show: function() { console.log(message); }};
 	toast.show(message, duration, position);
 };
+
+// Source: https://gist.github.com/jed/982883
+function uuidv4(
+  a                  // placeholder
+){
+  return a           // if the placeholder was passed, return
+    ? (              // a random number from 0 to 15
+      a ^            // unless b is 8,
+      Math.random()  // in which case
+      * 16           // a random number from
+      >> a/4         // 8 to 11
+      ).toString(16) // in hexadecimal
+    : (              // or otherwise a concatenated string:
+      [1e7] +        // 10000000 +
+      -1e3 +         // -1000 +
+      -4e3 +         // -4000 +
+      -8e3 +         // -80000000 +
+      -1e11          // -100000000000,
+      ).replace(     // replacing
+        /[018]/g,    // zeroes, ones, and eights with
+        uuidv4            // random hex digits
+      )
+}
